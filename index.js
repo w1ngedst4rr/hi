5f007103d575d814334b6027d4f3b200cd53e17b
⋆˚࿔ 𝐚𝐬𝐭é𝐫𝐢 ! 𝜗𝜚˚⋆✮ <b2029lsaayes@pshs.edu.ph>
2025-12-16 11:10:00 +0800

    Refactor CreateFlashcard and add fade-in effect
    Refactor flashcard creation logic and add fade-in effect on load.

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-let questionnum = 0
+window.addEventListener("load", () => {
+    const elements = document.querySelectorAll(".fade-in");
 
-function CreateFlashcard() {
-
-    questionnum += 1;
-
-    const question = document.getElementById("Question");
-    const answer = document.getElementById("Answer");
-
-    e.preventDefault();
-
-    const Qv = question.value;
-    const Av = answer.value;
-
-    localStorage.setItem('question' + questionnum, Qv)
-    localStorage.setItem('answer' + questionnum, Av)
-    
-    // put question and answer in localstorage
-
-    // put the q&a from localstorage -> display
-    // change id of the flashcard
-    // create id of flashcard
-
-}
+    elements.forEach((el, index) => {
+        setTimeout(() => {
+            el.classList.add("show");
+        }, index * 150);
+    });
+});
