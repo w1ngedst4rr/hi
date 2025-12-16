f0d69f1cd5c835ca929469d8e0342b00cdb7e836
⋆˚࿔ 𝐚𝐬𝐭é𝐫𝐢 ! 𝜗𝜚˚⋆✮ <b2029lsaayes@pshs.edu.ph>
2025-12-16 20:25:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-            let questionnum = 0
-            let container = document.getElementById('flashcard-container');
-            let qgrouplist = []
-            let agrouplist = []
-            let qgroup = [];
-            let agroup = [];
-            let pastY;
-
-            function update() {
-                localStorage.clear;
-            }
-
+function update() {
+    localStorage.clear();
 }
 
 window.addEventListener("load", () => {
     const elements = document.querySelectorAll(".fade-up");
-    elements.forEach((el, i) => {
+
+    elements.forEach((el, index) => {
         setTimeout(() => {
             el.classList.add("show");
-        }, i * 150);
+        }, index * 150);
     });
 });
-
-// put question and answer in localstorage
-
-    // put the q&a from localstorage -> display
-    //change id of the flashcard
-    // create id of flashcard
