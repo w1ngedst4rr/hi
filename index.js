f770100b602ea0f79002942b8d06c6151b051870
⋆˚࿔ 𝐚𝐬𝐭é𝐫𝐢 ! 𝜗𝜚˚⋆✮ <b2029lsaayes@pshs.edu.ph>
2025-12-16 20:39:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,4 @@ let qgrouplist = []
 let agrouplist = []
 let qgroup = [];
 let agroup = [];
-let pastY;
-
-function update() {
-    localStorage.clear();
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-    const elements = document.querySelectorAll(".fade-up");
-    elements.forEach((el, i) => {
-        setTimeout(() => {
-            el.classList.add("show");
-        }, i * 150);
-    });
-});
+let pastY
